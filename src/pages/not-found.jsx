881eec8e1fedbd0b4d69b9111dc93903c77197d5
Siddhu <siddhu@Rubus-MacBook-Pro.local>
2026-03-18 21:00:45 +0530

    updated tracking

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,13 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DGWV6LJKXL"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-DGWV6LJKXL');
-</script>
-
 import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
